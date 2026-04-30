c8dd9cb644f6ab839bbeafe21415a3e11cb8e108
Afebu Balogun <balogunafebu22@gmail.com>
2026-04-30 16:40:47 +0100

    refactor: streamline analyst seeding process and remove admin seeding logic

--- seed.py
+++ seed.py
@@ -1,32 +1,29 @@
+# Run this once in a shell/seed script
+# seed_analyst.py
 from app.db.database import SessionLocal
 from app.models.user_models import User
+from app.utils.tokens import create_access_token, create_refresh_token
 from uuid_extensions import uuid7str
+from datetime import datetime, timezone
 
 db = SessionLocal()
 
-# Seed Admin
-if not db.query(User).filter(User.role == "admin").first():
-    admin = User(
-        id=uuid7str(),
-        username="admin_tester",
-        email="admin@example.com",
-        role="admin",
-        is_active=True,
-        github_id=uuid7str()
-    )
-    db.add(admin)
-
-# Seed Analyst
-if not db.query(User).filter(User.role == "analyst").first():
+analyst = db.query(User).filter(User.role == "analyst").first()
+if not analyst:
     analyst = User(
         id=uuid7str(),
-        username="analyst_tester",
-        email="analyst@example.com",
+        github_id="test_analyst_001",
+        username="test_analyst",
+        email="analyst@test.com",
         role="analyst",
         is_active=True,
-        github_id=uuid7str()
+        last_login_at=datetime.now(timezone.utc),
     )
     db.add(analyst)
+    db.commit()
+    db.refresh(analyst)
 
-db.commit()
-print("Database seeded with Admin and Analyst!")
+token = create_access_token(analyst)
+refresh = create_refresh_token(db, analyst.id)
+print("Analyst token:", token)
+print("Refresh token:", refresh)
